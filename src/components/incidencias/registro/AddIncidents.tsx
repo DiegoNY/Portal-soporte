c6211052210880e06bedf55e984f22e3245044ae
Diego Raul <nalvartediego40@gmail.com>
2023-04-24 21:23:51 -0500

    portal para registro de incidencias creado

--- src/components/incidencias/registro/AddIncidents.tsx
+++ src/components/incidencias/registro/AddIncidents.tsx
@@ -1,65 +1,71 @@
 export const AddIncidents = () => {
   return (
-    <div className="col-span-full">
-      <h1 className="font-[500]">Nueva Incidencia N°23-7635</h1>
-      <p className="text-[14px]">completar todos los campos obligatorios</p>
-      <div className="flex  p-2 items-center">
-        <h1 className="w-[170px] text-slate-500">Empresa*</h1>
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-2  ">
-          <input
-            className="p-2 border-b bg-inherit outline-none"
-            placeholder="RUC EMPRESA"
-          />
-          <input
-            className="p-2 border-b bg-inherit outline-none"
-            placeholder="RAZON SOCIAL"
-          />
-          <select className="p-2 border-b outline-none bg-inherit">
-            <option>Seleccione sucursal</option>
-          </select>
+    <div className="absolute z-50 top-0 left-0 bg-transparent w-full h-full justify-center flex modal_style">
+      <div className=" fixed top-10 rounded-xl bg-white border ">
+        <div className="flex P-2">
+          <span className="px-5">X</span>
+          <h1 className="font-[500] text-center w-full">Nueva Incidencia N°23-7635</h1>
         </div>
-      </div>
-      <div className="flex  p-2 items-center">
-        <h1 className="w-[170px] text-slate-500">Contacto*</h1>
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-2  ">
-          <input
-            className="p-2 border-b bg-inherit outline-none"
-            placeholder="NOMBRE"
-          />
-          <input
-            className="p-2 border-b bg-inherit outline-none"
-            placeholder="DNI"
-          />
-          <select className="p-2 border-b outline-none bg-inherit">
-            <option>Seleccione</option>
-          </select>
-          <input
-            className="p-2 border-b bg-inherit outline-none"
-            placeholder="TELEFONO"
-          />
-          <input
-            className="p-2 border-b bg-inherit outline-none"
-            placeholder="CORREO"
-          />
-        </div>
-      </div>
-      <div className="flex  p-2 items-center">
-        <h1 className="w-[170px] text-slate-500">Tipo estacion*</h1>
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-2  ">
-          <select className="p-2 border-b outline-none bg-inherit">
-            <option>Seleccione sucursal</option>
-          </select>
-          <div>
-            <p>Prioridad</p>
+        <p className="text-[14px]">completar todos los campos obligatorios</p>
+        <div className="flex  p-2 items-center">
+          <h1 className="w-[170px] text-slate-500">Empresa*</h1>
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-2  ">
+            <input
+              className="p-2 border-b bg-inherit outline-none"
+              placeholder="RUC EMPRESA"
+            />
+            <input
+              className="p-2 border-b bg-inherit outline-none"
+              placeholder="RAZON SOCIAL"
+            />
             <select className="p-2 border-b outline-none bg-inherit">
               <option>Seleccione sucursal</option>
             </select>
           </div>
-          <select className="p-2 border-b outline-none bg-inherit">
-            <option>Seleccione sucursal</option>
-          </select>
+        </div>
+        <div className="flex  p-2 items-center">
+          <h1 className="w-[170px] text-slate-500">Contacto*</h1>
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-2  ">
+            <input
+              className="p-2 border-b bg-inherit outline-none"
+              placeholder="NOMBRE"
+            />
+            <input
+              className="p-2 border-b bg-inherit outline-none"
+              placeholder="DNI"
+            />
+            <select className="p-2 border-b outline-none bg-inherit">
+              <option>Seleccione</option>
+            </select>
+            <input
+              className="p-2 border-b bg-inherit outline-none"
+              placeholder="TELEFONO"
+            />
+            <input
+              className="p-2 border-b bg-inherit outline-none"
+              placeholder="CORREO"
+            />
+          </div>
+        </div>
+        <div className="flex  p-2 items-center">
+          <h1 className="w-[170px] text-slate-500">Tipo estacion*</h1>
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-2  ">
+            <select className="p-2 border-b outline-none bg-inherit">
+              <option>Seleccione sucursal</option>
+            </select>
+            <div>
+              <p>Prioridad</p>
+              <select className="p-2 border-b outline-none bg-inherit">
+                <option>Seleccione sucursal</option>
+              </select>
+            </div>
+            <select className="p-2 border-b outline-none bg-inherit">
+              <option>Seleccione sucursal</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
+
   );
 };
--- src/pages/home/incidencias/registro.tsx
+++ src/pages/home/incidencias/registro.tsx
@@ -3,25 +3,29 @@ import CardRegistroIncidencias from "@/components/incidencias/registro/CardRegis
 import { InformationRegister } from "@/components/incidencias/registro/InformationRegister/InformationRegister";
 import { PresentationIncidents } from "@/components/incidencias/registro/presentation_incidents/PresentationIncidents";
 import { RootState } from "@/libs/redux/store/store";
+import { createPortal } from "react-dom";
 import { useSelector } from "react-redux";
 
 const Registro = () => {
   const incidents = useSelector((state: RootState) => state.incidents);
 
   return (
-    <CardRegistroIncidencias>
-      {incidents.incident_record ? (
-        <>
-          <AddIncidents />
-        </>
-      ) : (
-        <>
-          <InformationRegister />
-          <PresentationIncidents />
-        </>
+    <>
+      {/* {incidents.incident_record ? (
+        <AddIncidents />
+      ) : null} */}
+
+      {incidents.incident_record && createPortal(
+        <AddIncidents />,
+        document.body
       )}
-    </CardRegistroIncidencias>
+      <CardRegistroIncidencias>
+        <InformationRegister />
+        <PresentationIncidents />
+      </CardRegistroIncidencias>
+    </>
+
   );
 };
 
-export default Registro;
+export default Registro;  
